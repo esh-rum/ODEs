6b279a45df5a8afc290b69924a6fde6f7c031fa5
Eshmal <eshmalrumman3@gmail.com>
2023-09-22 19:04:06 +0500

    added power rule method and also the if the power is -1 or 1/base in integration and it workssssss

--- integration calculator.py
+++ integration calculator.py
@@ -6,13 +6,24 @@ class Integration():
         
     def formulas(self):
         if "^" in self.function:
-            
-            
+            base, power = self.function.split("^")
+            if power.isdigit():
+                power = int(power)
+            if base == "x" or base == "y":
+                if power != "-1":
+                    power = power + 1
+                    base = f"({base} ^ {power})"
+                    result = f"{base} / {power} + c"
+                    return result 
+                else:
+                    result = f"ln({base}) + c"   
+                    return result    
         
         
 def main():
     func = input ("Enter function to be integrated: ")
-    result = Integration(func)
+    integ = Integration(func)
+    result = integ.formulas()
     
     print(result)
     
